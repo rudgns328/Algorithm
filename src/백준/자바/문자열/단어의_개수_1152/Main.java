cf36612b97fd0d24fb791c967f590089c5326f81
rudgns328 <rudgns328@gmail.com>
2024-03-12 16:50:07 +0900

    [Improve]-[백준] 단어의 개수 1152

--- src/백준/자바/문자열/단어의_개수_1152/Main.java
+++ src/백준/자바/문자열/단어의_개수_1152/Main.java
@@ -1,24 +1,14 @@
 package 백준.자바.문자열.단어의_개수_1152;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        int count = 0;
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ' ') {
-                count++;
-            }
-        }
-
-        if (str.charAt(0) != ' ' && str.charAt(str.length() - 1) != ' ') {
-            count += 1;
-        } else if (str.charAt(0) == ' ' && str.charAt(str.length() - 1) == ' ') {
-            count -= 1;
-        }
-        System.out.println(count);
+        StringTokenizer st = new StringTokenizer(str, " ");
+        System.out.println(st.countTokens());
     }
 }
