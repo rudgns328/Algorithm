0e14db62a61771e7c4a474344201433f54f1333d
rudgns328 <rudgns328@gmail.com>
2024-03-14 20:34:16 +0900

    [Solve]-[백준] 그대로 출력하기 11718

--- src/백준/자바/문자열/그대로_출력하기_11718/Main.java
+++ src/백준/자바/문자열/그대로_출력하기_11718/Main.java
@@ -1,2 +1,15 @@
-package 백준.자바.문자열.그대로_출력하기_11718;public class Main {
+package 백준.자바.문자열.그대로_출력하기_11718;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+        sc.close();
+    }
 }
