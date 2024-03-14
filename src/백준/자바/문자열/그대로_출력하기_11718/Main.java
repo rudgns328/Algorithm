284473d681d7159cbcf9274ca8552672f49e0b87
rudgns328 <rudgns328@gmail.com>
2024-03-14 20:32:30 +0900

    [Solve]-[백준] 다이얼 5622

---
+++ src/백준/자바/문자열/그대로_출력하기_11718/Main.java
@@ -0,0 +1,2 @@
+package 백준.자바.문자열.그대로_출력하기_11718;public class Main {
+}
---
+++ src/백준/자바/문자열/다이얼_5622/Main.java
@@ -0,0 +1,48 @@
+package 백준.자바.문자열.다이얼_5622;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String dial = sc.next();
+        int count = 0;
+
+        for(int i = 0; i < dial.length(); i++) {
+            switch(dial.charAt(i)) {
+                case 'A' : case 'B': case 'C' :
+                    count += 3;
+                    break;
+
+                case 'D' : case 'E': case 'F' :
+                    count += 4;
+                    break;
+
+                case 'G' : case 'H': case 'I' :
+                    count += 5;
+                    break;
+
+                case 'J' : case 'K': case 'L' :
+                    count += 6;
+                    break;
+
+                case 'M' : case 'N': case 'O' :
+                    count += 7;
+                    break;
+
+                case 'P' : case 'Q': case 'R' : case 'S' :
+                    count += 8;
+                    break;
+
+                case 'T' : case 'U': case 'V' :
+                    count += 9;
+                    break;
+
+                case 'W' : case 'X': case 'Y' : case 'Z' :
+                    count += 10;
+                    break;
+            }
+        }
+        System.out.println(count);
+    }
+}
