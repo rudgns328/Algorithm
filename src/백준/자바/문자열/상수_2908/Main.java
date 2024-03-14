eb18fd81b7e187e99de7adf2844f63cc00bd2d2a
rudgns328 <rudgns328@gmail.com>
2024-03-14 20:33:51 +0900

    [Solve]-[백준] 상수 2908

--- src/백준/자바/문자열/상수_2908/Main.java
+++ src/백준/자바/문자열/상수_2908/Main.java
@@ -1,2 +1,16 @@
-package 백준.자바.문자열.상수_2908;public class Main {
+package 백준.자바.문자열.상수_2908;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        num1 = Integer.parseInt(new StringBuilder().append(num1).reverse().toString());
+        num2 = Integer.parseInt(new StringBuilder().append(num2).reverse().toString());
+
+        System.out.println(num1 > num2 ? num1 : num2);
+    }
 }
