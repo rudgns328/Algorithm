fffc36b853462840e764d75779deb6a0b507165e
rudgns328 <rudgns328@gmail.com>
2024-03-21 23:28:32 +0900

    [Improve]-[백준] 성 지키기 1236

--- src/백준/자바/배열/성_지키기_1236/Main.java
+++ src/백준/자바/배열/성_지키기_1236/Main.java
@@ -3,7 +3,7 @@ package 백준.자바.배열.성_지키기_1236;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
