1979cfb7bc81314185d91774569320399225db1d
rudgns328 <rudgns328@gmail.com>
2024-03-22 18:07:00 +0900

    [Solve]-[백준] 빠른 A+B 15552

--- src/백준/자바/수학/빠른A더하기B_15552/Main.java
+++ src/백준/자바/수학/빠른A더하기B_15552/Main.java
@@ -1,2 +1,24 @@
-package 백준.자바.수학.빠른A더하기B_15552;public class Main {
+package 백준.자바.수학.빠른A더하기B_15552;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
