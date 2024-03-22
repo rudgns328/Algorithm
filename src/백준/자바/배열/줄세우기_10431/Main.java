27051e04574c3c3478d764eb160a54acc21fa640
rudgns328 <rudgns328@gmail.com>
2024-03-22 18:05:16 +0900

    [Solve]-[백준] 줄세우기 10431

--- src/백준/자바/배열/줄세우기_10431/Main.java
+++ src/백준/자바/배열/줄세우기_10431/Main.java
@@ -1,2 +1,36 @@
-package 백준.자바.배열.줄세우기_10431;public class Maiin {
+package 백준.자바.배열.줄세우기_10431;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int P = sc.nextInt();
+
+        while (P-- > 0) {
+            int T = sc.nextInt();
+            int[] h = new int[20];
+            for (int i = 0; i < 20; i++)
+                h[i] = sc.nextInt();
+
+            int cnt = 0;
+            int[] sorted = new int[20];
+            for (int i = 0; i < 20; i++) {
+                boolean find = false;
+                for (int j = 0; j < i; j++)
+                    if (sorted[j] > h[i]) {
+                        find = true;
+
+                        for (int k = i - 1; k >= j; k--) {
+                            sorted[k + 1] = sorted[k];
+                            cnt ++;
+                        }
+                        sorted[j] = h[i];
+                        break;
+                    }
+                if (!find) sorted[i] = h[i];
+            }
+            System.out.println(T + " " + cnt);
+        }
+    }
 }
