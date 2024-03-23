e78e4242a9d7048b9fbd8cb0b4741fa748dcb210
rudgns328 <rudgns328@gmail.com>
2024-03-23 22:54:53 +0900

    [Solve]-[백준] 주사위 세개 2480

--- src/백준/자바/조건문/주사위_세개_2480/Main.java
+++ src/백준/자바/조건문/주사위_세개_2480/Main.java
@@ -1,2 +1,33 @@
-package 백준.자바.조건문.주사위_세개_2480;public class Main {
+package 백준.자바.조건문.주사위_세개_2480;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (arr[0] == arr[1] && arr[0] == arr[2]) {
+            int ans = 10000 + arr[0] * 1000;
+            System.out.println(ans);
+        } else if (arr[0] == arr[1] || arr[0] == arr[2]) {
+            int ans = 1000 + arr[0] * 100;
+            System.out.println(ans);
+        } else if (arr[1] == arr[2]) {
+            int ans = 1000 + arr[1] * 100;
+            System.out.println(ans);
+        } else if (arr[0] != arr[1] && arr[0] != arr[2] && arr[1] != arr[2]) {
+            int max = Math.max(arr[0], Math.max(arr[1], arr[2]));
+            int ans = max * 100;
+            System.out.println(ans);
+        }
+    }
 }
