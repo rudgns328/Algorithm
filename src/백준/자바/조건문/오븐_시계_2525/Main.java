f83c9b9b5540f328b9f5ce1d800157a55f0828b9
rudgns328 <rudgns328@gmail.com>
2024-03-23 22:50:17 +0900

    [Solve]-[백준] 오븐 시계 2525

--- src/백준/자바/조건문/오븐_시계_2525/Main.java
+++ src/백준/자바/조건문/오븐_시계_2525/Main.java
@@ -1,2 +1,24 @@
-package 백준.자바.조건문.오븐_시계_2525;public class Main {
+package 백준.자바.조건문.오븐_시계_2525;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] currentTime = sc.nextLine().split(" ");
+        int currentHour = Integer.parseInt(currentTime[0]);
+        int currentMinute = Integer.parseInt(currentTime[1]);
+        int currentMinuteAmount = currentHour * 60 + currentMinute;
+
+        int needTime = sc.nextInt();
+
+        int answerTime = currentMinuteAmount + needTime;
+        if (answerTime >= 1440)
+            answerTime -= 1440;
+
+        int answerHour = answerTime / 60;
+        int answerMinute = answerTime % 60;
+
+        System.out.println(answerHour + " " + answerMinute);
+    }
 }
