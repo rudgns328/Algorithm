8241ca056075f1a858f9f48b0a0ae6c727db455c
rudgns328 <rudgns328@gmail.com>
2024-03-23 22:47:25 +0900

    [Solve]-[백준] 두 수의 합 3273

--- src/백준/자바/배열/두_수의_합_3273/Main.java
+++ src/백준/자바/배열/두_수의_합_3273/Main.java
@@ -1,7 +1,39 @@
-package 백준.자바.배열.두_수의__3273;
+package 백준.자바.배열.두_수의_합_3273;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        int X = Integer.parseInt(br.readLine());
+        int start = 0;
+        int end = N - 1;
+        int sum = 0;
+        int count = 0;
+
+        while (start < end) {
+            sum = arr[start] + arr[end];
+            if (sum < X) {
+                start++;
+            } else if (sum > X) {
+                end--;
+            } else {
+                count++;
+                start++;
+                end--;
+            }
+        }
+        System.out.println(count);
     }
 }
