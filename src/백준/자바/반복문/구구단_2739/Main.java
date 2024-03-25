1a6bcac2d14eb9cf79b1cde4e1136be6f0aa84d4
rudgns328 <rudgns328@gmail.com>
2024-03-25 21:05:16 +0900

    [Solve]-[백준] 구구단 2739

--- src/백준/자바/반복문/구구단_2739/Main.java
+++ src/백준/자바/반복문/구구단_2739/Main.java
@@ -1,2 +1,14 @@
-package 백준.자바.반복문.구구단_2739;public class Main {
+package 백준.자바.반복문.구구단_2739;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <=9; i++) {
+            System.out.println(N + " * " + i + " = " + (N * i));
+        }
+    }
 }
