a271a4c3a120ea1fa2e121887abd62c5e8b4064b
rudgns328 <rudgns328@gmail.com>
2024-03-29 23:10:32 +0900

    [Solve]-[백준] 코딩은 체육과목 입니다 25314

--- src/백준/자바/반복문/코딩은_체육과목_입니다_25314/Main.java
+++ src/백준/자바/반복문/코딩은_체육과목_입니다_25314/Main.java
@@ -1,2 +1,21 @@
-package 백준.자바.반복문.코딩은_체육과목_입니다_25314;public class Main {
+package 백준.자바.반복문.코딩은_체육과목_입니다_25314;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        String str = "";
+        scan.close();
+
+        for(int i=1; i<=n/4; i++) {
+            str += "long ";
+        }
+        System.out.println(str + "int");
+
+    }
 }
+
