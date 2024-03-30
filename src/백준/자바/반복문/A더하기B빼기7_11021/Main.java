218a6032c7370e3879c14f5130ce887b90288173
rudgns328 <rudgns328@gmail.com>
2024-03-30 23:47:00 +0900

    [Solve]-[백준] A+B-7 11021

---
+++ src/백준/자바/반복문/A더하기B빼기7_11021/Main.java
@@ -0,0 +1,21 @@
+package 백준.자바.반복문.A더하기B빼기7_11021;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            int c = in.nextInt();
+            int d = in.nextInt();
+
+            System.out.println("Case #" + i + ": " + (c + d));
+        }
+
+        in.close();
+    }
+}
---
+++ src/백준/자바/반복문/A더하기B빼기8_11022/Main.java
@@ -0,0 +1,2 @@
+package 백준.자바.반복문.A더하기B빼기8_11022;public class Main {
+}
