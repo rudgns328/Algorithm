1b686765f706d0d95519b864ca73dba4a955c914
rudgns328 <rudgns328@gmail.com>
2024-03-31 23:44:55 +0900

    [Solve]-[백준] 별 찍기 2 2439

--- src/백준/자바/반복문/별_찍기_2_2439/Main.java
+++ src/백준/자바/반복문/별_찍기_2_2439/Main.java
@@ -1,2 +1,23 @@
-package 백준.자바.반복문.별_찍기_2_2439;public class Main {
+package 백준.자바.반복문.별_찍기_2_2439;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        in.close();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
