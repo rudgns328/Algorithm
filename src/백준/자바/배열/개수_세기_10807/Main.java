b61fc489c9275ce8a1279ef331998b9ba264dba4
rudgns328 <rudgns328@gmail.com>
2024-04-05 23:48:41 +0900

    [Solve]-[백준] 개수 세기 10807

--- src/백준/자바/배열/개수_세기_10807/Main.java
+++ src/백준/자바/배열/개수_세기_10807/Main.java
@@ -1,2 +1,25 @@
-package 백준.자바.배열.개수_세기_10807;public class Main {
+package 백준.자바.배열.개수_세기_10807;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int b = sc.nextInt();
+
+        for(int j = 0; j < arr.length; j++){
+            if(b == arr[j]){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
