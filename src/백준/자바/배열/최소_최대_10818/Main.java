62a7414c849d5d9f2c6b27b4dbce560ccfdeaf6a
rudgns328 <rudgns328@gmail.com>
2024-04-06 23:36:25 +0900

    [Solve]-[백준] 최소, 최대 10818

--- src/백준/자바/배열/최소_최대_10818/Main.java
+++ src/백준/자바/배열/최소_최대_10818/Main.java
@@ -1,2 +1,22 @@
-package 백준.자바.배열.최소_최대_10818;public class Main {
+package 백준.자바.배열.최소_최대_10818;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        in.close();
+        Arrays.sort(arr);
+        System.out.print(arr[0] + " " + arr[N - 1]);
+    }
 }
