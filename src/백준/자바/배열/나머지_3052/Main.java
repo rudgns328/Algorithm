fe02615a799cd63c8c81ebc7e3ef393d5eca5b76
rudgns328 <rudgns328@gmail.com>
2024-04-10 23:51:12 +0900

    [Solve]-[백준] 나머지 3052

--- src/백준/자바/배열/나머지_3052/Main.java
+++ src/백준/자바/배열/나머지_3052/Main.java
@@ -1,2 +1,18 @@
-package 백준.자바.배열.나머지_3052;public class Main {
+package 백준.자바.배열.나머지_3052;
+
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        HashSet<Integer> h = new HashSet<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            h.add(in.nextInt() % 42);
+        }
+
+        in.close();
+        System.out.print(h.size());
+    }
 }
