6e68b6f3af4bcb7574bc9b5ca7be2312ed696f31
rudgns328 <rudgns328@gmail.com>
2024-04-11 21:44:19 +0900

    [Solve]-[백준] 평균_1546

--- src/백준/자바/배열/평균_1546/Main.java
+++ src/백준/자바/배열/평균_1546/Main.java
@@ -1,2 +1,30 @@
-package 백준.자바.배열.평균_1546;public class Main {
+package 백준.자바.배열.평균_1546;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] score = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            score[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            if (score[i] > max) {
+                max = score[i];
+            }
+        }
+
+        double sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += (double) score[i] / max * 100;
+        }
+
+        double ans = sum / N;
+        System.out.println(ans);
+    }
 }
