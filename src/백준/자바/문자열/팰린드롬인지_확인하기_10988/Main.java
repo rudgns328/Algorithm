d2eaf8f81c2c94c345182ae4a2c192568579bbc5
rudgns328 <rudgns328@gmail.com>
2024-04-12 21:07:18 +0900

    [Solve]-[백준] 팰린드롬인제 확인하기 10988

--- src/백준/자바/문자열/팰린드롬인지_확인하기_10988/Main.java
+++ src/백준/자바/문자열/팰린드롬인지_확인하기_10988/Main.java
@@ -1,2 +1,17 @@
-package 백준.자바.문자열.팰린드롬인지_확인하기_10988;public class Main {
+package 백준.자바.문자열.팰린드롬인지_확인하기_10988;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        StringBuffer sb = new StringBuffer(str);
+        String reverse = sb.reverse().toString();
+
+        if (str.equals(reverse)) {
+            System.out.println(1);
+        } else System.out.println(0);
+    }
 }
