8adad8c935ca284d1c88ca65935bbb5e4102bfef
rudgns328 <rudgns328@gmail.com>
2024-04-15 22:30:59 +0900

    [Improve]-[백준] 행렬 덧셈 2738

--- src/백준/자바/배열/행렬_덧셈_2738/Main.java
+++ src/백준/자바/배열/행렬_덧셈_2738/Main.java
@@ -1,29 +1,39 @@
 package 백준.자바.배열.행렬_덧셈_2738;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n][m];
-        for (int i = 0; i < n; i++) {
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr1 = new int[N][M];
+        int[][] arr2 = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                arr1[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                sb.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
+            for (int j = 0; j < M; j++) {
+                arr2[i][j] = Integer.parseInt(st.nextToken());
             }
-            sb.append("\n");
         }
-        System.out.println(sb);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(arr1[i][j] + arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+        br.close();
     }
 }
