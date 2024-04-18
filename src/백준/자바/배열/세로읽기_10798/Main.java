0539f7b40434895119114cba4e7fec4c51138c09
rudgns328 <rudgns328@gmail.com>
2024-04-18 23:35:55 +0900

    [Improve]-[백준] 세로읽기 10798

--- src/백준/자바/배열/세로읽기_10798/Main.java
+++ src/백준/자바/배열/세로읽기_10798/Main.java
@@ -1,30 +1,26 @@
 package 백준.자바.배열.세로읽기_10798;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        char[][] arr = new char[5][15];
 
-        char[][] str = new char[5][15];
-
-        String input = "";
-        for (int i = 0; i < str.length; i++) {
-            input = sc.next();
-            // 15개 문자 넣기
-            for (int j = 0; j < input.length(); j++) {
-                str[i][j] = input.charAt(j);
+        String str = "";
+        for (int i = 0; i < arr.length; i++) {
+            str = sc.next();
+            for (int j = 0; j < str.length(); j++) {
+                arr[i][j] = str.charAt(j);
             }
         }
 
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 5; j++) {
-                if (str[j][i] == '\0')
+                if (arr[j][i] == '\0')
                     continue;
-                System.out.print(str[j][i]);
-
+                System.out.print(arr[j][i]);
             }
         }
     }
