f0a29d8f19fa8aa560c3df6546b55d35a6fe3742
rudgns328 <rudgns328@gmail.com>
2024-04-20 01:27:13 +0900

    [Solve]-[백준] 달팽이는 올라가고 싶다 2869

--- src/백준/자바/수학/달팽이는_올라가고_싶다_2869/Main.java
+++ src/백준/자바/수학/달팽이는_올라가고_싶다_2869/Main.java
@@ -1,2 +1,23 @@
-package 백준.자바.수학.달팽이는_올라가고_싶다_2869;public class Main {
+package 백준.자바.수학.달팽이는_올라가고_싶다_2869;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int up = in.nextInt();		// A
+        int down = in.nextInt();	// B
+        int length = in.nextInt(); 	// C
+
+        int day = (length - down) / (up - down);
+
+        // 나머지가 있을 경우 (잔여 블럭이 있을 경우)
+        if ((length - down) % (up - down) != 0) {
+            day++;
+        }
+        System.out.println(day);
+    }
 }
