dc0dd47eea4ac635335957e3ceafddc73840315c
rudgns328 <rudgns328@gmail.com>
2024-04-19 13:47:57 +0900

    [Solve]-[백준] 세탁소 사장 동혁 2720

--- src/백준/자바/수학/세탁소_사장_동혁_2720/Main.java
+++ src/백준/자바/수학/세탁소_사장_동혁_2720/Main.java
@@ -1,2 +1,25 @@
-package 백준.자바.수학.세탁소_사장_동혁_2720;public class Main {
+package 백준.자바.수학.세탁소_사장_동혁_2720;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int quarter = 0;
+        int dime = 0;
+        int nickel = 0;
+        int penny = 0;
+
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+
+            quarter = a / 25;
+            dime = (a % 25) / 10;
+            nickel = (a % 25 % 10) / 5;
+            penny = (a % 25 % 10) % 5;
+
+            System.out.println(quarter + " " + dime + " " + nickel + " " + penny);
+        }
+    }
 }
