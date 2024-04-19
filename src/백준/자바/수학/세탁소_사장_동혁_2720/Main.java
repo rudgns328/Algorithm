1dda16057264736d6e47758ae26a7bd9144c142c
rudgns328 <rudgns328@gmail.com>
2024-04-19 13:44:06 +0900

    [Improve]-[백준] 진법 변환 2745

---
+++ src/백준/자바/수학/세탁소_사장_동혁_2720/Main.java
@@ -0,0 +1,2 @@
+package 백준.자바.수학.세탁소_사장_동혁_2720;public class Main {
+}
--- src/백준/자바/수학/진법_변환_2745/Main.java
+++ src/백준/자바/수학/진법_변환_2745/Main.java
@@ -7,16 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int N = sc.nextInt();
-        int ten = 0;
-        long ans = 0;
-        int idx = 0;
 
-        for (int i = str.length() - 1; i >= 0; i--) {
-            if ('0' <= str.charAt(i) && str.charAt(i) <= '9') {
-                ten = str.charAt(i) - '0';
-            } else ten = str.charAt(i) - 55;
-            ans += ten * Math.pow(N, idx++);
-        }
-        System.out.println(ans);
+        System.out.println(Integer.parseInt(str, N));
     }
 }
