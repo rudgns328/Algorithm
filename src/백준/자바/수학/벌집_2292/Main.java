2a0c293ff99c8fe21d534786ef876fddf4d71c96
rudgns328 <rudgns328@gmail.com>
2024-04-19 19:21:42 +0900

    [Solve]-[백준] 벌집 2292

--- src/백준/자바/수학/벌집_2292/Main.java
+++ src/백준/자바/수학/벌집_2292/Main.java
@@ -1,2 +1,21 @@
-package 백준.자바.수학.벌집_2292;public class Main {
+package 백준.자바.수학.벌집_2292;
+
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int range = 2;
+        int count = 1;
+
+        while(range <= N) {
+            if (N == 1)
+                System.out.println(1);
+            else {
+                range = range + (6 * count);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
