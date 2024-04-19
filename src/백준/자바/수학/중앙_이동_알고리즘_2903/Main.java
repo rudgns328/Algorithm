72a850a7884592c1c9f23a7f1120bcdcf94063b3
rudgns328 <rudgns328@gmail.com>
2024-04-19 19:21:25 +0900

    [Solve]-[백준] 중앙 이동 알고리즘 2903

--- src/백준/자바/수학/중앙_이동_알고리즘_2903/Main.java
+++ src/백준/자바/수학/중앙_이동_알고리즘_2903/Main.java
@@ -1,2 +1,13 @@
-package 백준.자바.수학.중앙_이동_알고리즘_2903;public class Main {
+package 백준.자바.수학.중앙_이동_알고리즘_2903;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int ans = (int)Math.pow((Math.pow(2, N) + 1), 2);
+        System.out.println(ans);
+        br.close();
+    }
 }
