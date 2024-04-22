35fd94cef561b5274c7a9be06ad8de371fc12c7f
rudgns328 <rudgns328@gmail.com>
2024-04-22 23:21:57 +0900

    [Improve]-[백준] 달팽이는 올라가고 싶다 2869

--- src/백준/자바/수학/달팽이는_올라가고_싶다_2869/Main.java
+++ src/백준/자바/수학/달팽이는_올라가고_싶다_2869/Main.java
@@ -1,20 +1,20 @@
 package 백준.자바.수학.달팽이는_올라가고_싶다_2869;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-
-        int up = in.nextInt();		// A
-        int down = in.nextInt();	// B
-        int length = in.nextInt(); 	// C
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int up = Integer.parseInt(st.nextToken());
+        int down = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(st.nextToken());
 
         int day = (length - down) / (up - down);
-
-        // 나머지가 있을 경우 (잔여 블럭이 있을 경우)
         if ((length - down) % (up - down) != 0) {
             day++;
         }
