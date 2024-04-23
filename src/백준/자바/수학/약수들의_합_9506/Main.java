3f2bbd3fa6743fb27eae24d0004dc16e132c3942
rudgns328 <rudgns328@gmail.com>
2024-04-23 16:52:15 +0900

    [Solve]-[백준] 약수들의 합 9506

--- src/백준/자바/수학/약수들의_합_9506/Main.java
+++ src/백준/자바/수학/약수들의_합_9506/Main.java
@@ -1,2 +1,30 @@
-package 백준.자바.수학.약수들의_합_9506;public class Main {
+package 백준.자바.수학.약수들의_합_9506;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringBuilder sb = new StringBuilder();
+            int sum = 1;
+            int N = Integer.parseInt(br.readLine());
+            if (N == -1) break;
+
+            sb.append(N + " = 1");
+            for (int i = 2; i < N; i++) {
+                if (N % i == 0) {
+                    sb.append(" + " + i);
+                    sum += i;
+                }
+            }
+
+            if (sum != N) {
+                System.out.println(N + " is NOT perfect.");
+            } else System.out.println(sb);
+        }
+    }
 }
