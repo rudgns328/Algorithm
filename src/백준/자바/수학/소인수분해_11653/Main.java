637c26e00c5d3158bf28bb7676ac0f9ca52da103
rudgns328 <rudgns328@gmail.com>
2024-04-24 23:38:28 +0900

    [Solve]-[백준] 소인수분해 11653

--- src/백준/자바/수학/소인수분해_11653/Main.java
+++ src/백준/자바/수학/소인수분해_11653/Main.java
@@ -1,2 +1,21 @@
-package 백준.자바.수학.소인수분해_11653;public class Main {
+package 백준.자바.수학.소인수분해_11653;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {	// 또는 i * i <= N
+            while (N % i == 0) {
+                System.out.println(i);
+                N /= i;
+            }
+        }
+        if (N != 1) {
+            System.out.println(N);
+        }
+    }
 }
