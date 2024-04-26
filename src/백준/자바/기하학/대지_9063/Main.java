98fcdbd9c97f25296d9fd18513bd60f814a43076
rudgns328 <rudgns328@gmail.com>
2024-04-26 22:21:13 +0900

    [Solve]-[백준] 대지 9063

--- src/백준/자바/기하학/대지_9063/Main.java
+++ src/백준/자바/기하학/대지_9063/Main.java
@@ -1,2 +1,30 @@
-package 백준.자바.기하학.대지_9063;public class Main {
+package 백준.자바.기하학.대지_9063;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        //정렬
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        //정렬했기때문에 끝과 끝이 제일 큰수와 제일 작은 수가 된다.
+        System.out.println((A[N - 1] - A[0]) * (B[N - 1] - B[0]));
+    }
+
 }
