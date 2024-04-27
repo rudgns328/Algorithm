2a30ac6abbc5c1d554f9e606f4ad334d236d17d2
rudgns328 <rudgns328@gmail.com>
2024-04-27 16:31:42 +0900

    [Solve]-[백준] 세 막대 14215

--- src/백준/자바/기하학/세_막대_14215/Main.java
+++ src/백준/자바/기하학/세_막대_14215/Main.java
@@ -1,2 +1,29 @@
-package 백준.자바.기하학.세_막대_14215;public class main {
+package 백준.자바.기하학.세_막대_14215;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int maxN = 0;
+        if(a>b){
+            if(b>c) maxN = a;
+            else maxN = (a>c) ? a : c;
+        }
+        else maxN = (b>c) ? b : c;
+
+        if(a+b+c-maxN > maxN) System.out.println(a+b+c);
+        else System.out.print((a+b+c-maxN)*2-1);
+
+        br.close();
+    }
 }
