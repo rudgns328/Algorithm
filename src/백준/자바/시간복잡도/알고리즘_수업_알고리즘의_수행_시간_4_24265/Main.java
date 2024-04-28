00c66fbbf9e49062acb8b7d611dfa55302ccec99
rudgns328 <rudgns328@gmail.com>
2024-04-29 02:26:25 +0900

    [Solve]-[백준] 알고리즘 수업 - 알고리즘의 수행 시간 4 24265

--- src/백준/자바/시간복잡도/알고리즘_수업_알고리즘의_수행_시간_4_24265/Main.java
+++ src/백준/자바/시간복잡도/알고리즘_수업_알고리즘의_수행_시간_4_24265/Main.java
@@ -1,2 +1,24 @@
-package 백준.자바.시간복잡도.알고리즘_수업_알고리즘의_수행_시간_4_24265;public class Main {
+package 백준.자바.시간복잡도.알고리즘_수업_알고리즘의_수행_시간_4_24265;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        for(int i = 1; i <= n-1; i++){
+            for(int j = i+1;  j <= n; j++){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
 }
