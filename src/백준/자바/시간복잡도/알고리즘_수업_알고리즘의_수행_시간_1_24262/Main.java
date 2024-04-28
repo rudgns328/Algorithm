63424b376389cd0cd4c92dcf8da592546531fbf5
rudgns328 <rudgns328@gmail.com>
2024-04-28 23:40:11 +0900

    [Solve]-[백준] 알고리즘 수업 - 알고리즘의 수행 시간 1 24262

--- src/백준/자바/시간복잡도/알고리즘_수업_알고리즘의_수행_시간_1_24262/Main.java
+++ src/백준/자바/시간복잡도/알고리즘_수업_알고리즘의_수행_시간_1_24262/Main.java
@@ -1,4 +1,9 @@
-package 백준.자바.시간복잡도.알고리즘_수업_알고리즘의_수행_시간_1;
+package 백준.자바.시간복잡도.알고리즘_수업_알고리즘의_수행_시간_1_24262;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
 }
+
