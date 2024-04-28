78e248c8d228f81ead4e49a9a5dd9bef4825ac2f
rudgns328 <rudgns328@gmail.com>
2024-04-28 23:38:10 +0900

    [Solve]-[백준] 알고리즘 수업 - 알고리즘의 수행 시간 2 24262

---
+++ src/백준/자바/시간복잡도/알고리즘_수업_알고리즘의_수행_시간_2_24263/Main.java
@@ -0,0 +1,16 @@
+package 백준.자바.시간복잡도.알고리즘_수업_알고리즘의_수행_시간_2_24263;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); //입력 크기 입력받음 => 이것이 수행 홧수
+        br.close();
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
