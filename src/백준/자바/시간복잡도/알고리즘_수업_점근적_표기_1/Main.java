23700faa1c07f8fad3516efccea67741a350af02
rudgns328 <rudgns328@gmail.com>
2024-05-03 23:31:35 +0900

    [Solve]-[백준] 알고리즘 수업 - 점근적 표기 1

--- src/백준/자바/시간복잡도/알고리즘_수업_점근적_표기_1/Main.java
+++ src/백준/자바/시간복잡도/알고리즘_수업_점근적_표기_1/Main.java
@@ -1,2 +1,23 @@
-package 백준.자바.시간복잡도.알고리즘_수업_점근적_표기_1;public class Main {
+package 백준.자바.시간복잡도.알고리즘_수업_점근적_표기_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+
+        if((a1*n0 + a0 <= c*n0) && c>=a1) System.out.println(1);
+        else System.out.println(0);
+
+        br.close();
+    }
 }
