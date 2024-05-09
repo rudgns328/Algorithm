19755d74af6003fd4ec233e7a953dd38c811970b
rudgns328 <rudgns328@gmail.com>
2024-05-09 20:44:02 +0900

    [Solve]-[백준] 영화감독 숌 1436

--- src/백준/자바/브루트포스/영화감독_숌_1436/Main.java
+++ src/백준/자바/브루트포스/영화감독_숌_1436/Main.java
@@ -1,2 +1,22 @@
-package 백준.자바.브루트포스.영화감독_숌_1436;public class Main {
+package 백준.자바.브루트포스.영화감독_숌_1436;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while(count != N) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
 }
