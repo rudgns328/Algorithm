69a99cd56b69ca56f179181885e1d0ca0f1a8431
rudgns328 <rudgns328@gmail.com>
2024-05-13 21:34:00 +0900

    [Improve]-[백준] 소수 2581

--- src/백준/자바/수학/소수_2581/Main.java
+++ src/백준/자바/수학/소수_2581/Main.java
@@ -5,27 +5,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
         int N = sc.nextInt();
-        int count = 0;
+        int min = N;
+        int sum = 0;
 
-        for (int i = 0; i < N; i++) {
-            int num = sc.nextInt();
-            if (isPrime(num)) {
-                count++;
+        for (int i = M; i <= N; i++) {
+            boolean check = true;
+
+            if (i == 1) check = false;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                if (min > i) min = i;
+                sum += i;
             }
         }
-        System.out.println(count);
-    }
-
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
+        if (sum == 0) System.out.println(-1);
+        else {
+            System.out.println(sum);
+            System.out.println(min);
         }
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
