bac440f3820dc067d686acde5ecbaa07aad24c47
rudgns328 <rudgns328@gmail.com>
2024-05-13 21:34:19 +0900

    [Improve]-[백준] 소인수분해 11653

--- src/백준/자바/수학/소인수분해_11653/Main.java
+++ src/백준/자바/수학/소인수분해_11653/Main.java
@@ -8,14 +8,16 @@ public class Main {
 
         int N = in.nextInt();
 
-        for (int i = 2; i <= Math.sqrt(N); i++) {	// 또는 i * i <= N
+        for (int i = 2; i * i <= N; i++) {
             while (N % i == 0) {
                 System.out.println(i);
                 N /= i;
             }
         }
+
         if (N != 1) {
             System.out.println(N);
         }
+
     }
 }
