46c09fc94092ef9e4f206c2b05ca0513e4e9e43a
rudgns328 <rudgns328@gmail.com>
2024-05-15 21:21:03 +0900

    [Improve]-[백준] 수학은 체육과목 입니다 15894

--- src/백준/자바/기하학/수학은_체육과목_입니다_15894/Main.java
+++ src/백준/자바/기하학/수학은_체육과목_입니다_15894/Main.java
@@ -1,12 +1,11 @@
 package 백준.자바.기하학.수학은_체육과목_입니다_15894;
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long n = Long.parseLong(br.readLine()); //학교의 수
+        long n = Long.parseLong(br.readLine());
         System.out.print(4 * n);
     }
 }
