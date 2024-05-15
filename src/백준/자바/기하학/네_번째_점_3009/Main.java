ff0b0ad52b5fa611617346749437846b02b0dbe3
rudgns328 <rudgns328@gmail.com>
2024-05-15 21:20:32 +0900

    [Improve]-[백준] 네 번째 점 3009

--- src/백준/자바/기하학/네_번째_점_3009/Main.java
+++ src/백준/자바/기하학/네_번째_점_3009/Main.java
@@ -5,48 +5,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] A = { sc.nextInt(), sc.nextInt() };
+        int[] B = { sc.nextInt(), sc.nextInt() };
+        int[] C = { sc.nextInt(), sc.nextInt() };
+        int X = 0;
+        int Y = 0;
 
-        Scanner in = new Scanner(System.in);
+        if (A[0] == B[0]) {
+            X = C[0];
+        } else if (A[0] == C[0]) {
+            X = B[0];
+        } else X = A[0];
 
-        int[] coord_1 = { in.nextInt(), in.nextInt() };	// 첫 번째 좌표
-        int[] coord_2 = { in.nextInt(), in.nextInt() };	// 두 번째 좌표
-        int[] coord_3 = { in.nextInt(), in.nextInt() };	// 세 번째 좌표
+        if (A[1] == B[1]) {
+            Y = C[1];
+        } else if (A[1] == C[1]) {
+            Y = B[1];
+        } else Y = A[1];
 
-
-        in.close();
-
-        int x;
-        int y;
-
-        // x 좌표 비교 후 쌍을 이루지 않는 x좌표를 저장
-        // 1번 x좌표와 2번 x좌표 비교
-        if (coord_1[0] == coord_2[0]) {
-            x = coord_3[0];
-        }
-        // 1번 x좌표와 3번 x좌표 비교
-        else if (coord_1[0] == coord_3[0]) {
-            x = coord_2[0];
-        }
-        // 2번 x좌표와 3번 x좌표 비교
-        else {
-            x = coord_1[0];
-        }
-
-        // y 좌표 비교
-        // 1번 y좌표와 2번 y좌표 비교
-        if (coord_1[1] == coord_2[1]) {
-            y = coord_3[1];
-        }
-        // 1번 y좌표와 3번 y좌표 비교
-        else if (coord_1[1] == coord_3[1]) {
-            y = coord_2[1];
-        }
-        // 2번 y좌표와 3번 y좌표 비교
-        else {
-            y = coord_1[1];
-        }
-
-        System.out.println(x + " " + y);
-
+        System.out.println(X + " " + Y);
     }
 }
