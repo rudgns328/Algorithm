b82c5384565c55f032cfda011bf71d9dacf62f17
rudgns328 <rudgns328@gmail.com>
2024-05-16 23:52:29 +0900

    [Improve]-[백준] 삼각형 외우기 10101

--- src/백준/자바/기하학/삼각형_외우기_10101/Main.java
+++ src/백준/자바/기하학/삼각형_외우기_10101/Main.java
@@ -1,7 +1,6 @@
 package 백준.자바.기하학.삼각형_외우기_10101;
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,16 +8,15 @@ public class Main {
         int a = Integer.parseInt(br.readLine());
         int b = Integer.parseInt(br.readLine());
         int c = Integer.parseInt(br.readLine());
-        if (a + b + c != 180) {
-            System.out.print("Error");
-        } else {
-            if (a == 60 && b == 60 && c == 60) {
-                System.out.print("Equilateral");
-            } else if (a == b || b == c || c == a) {
-                System.out.print("Isosceles");
-            } else {
-                System.out.print("Scalene");
-            }
+
+        if (a == 60 && b == 60 && c == 60) {
+            System.out.println("Equilateral");
+        } else if (a + b + c == 180 && (a == b || a == c || b == c)) {
+            System.out.println("Isosceles");
+        } else if (a + b + c == 180 && (a != b && a != c && b != c)) {
+            System.out.println("Scalene");
+        } else if (a + b + c != 180) {
+            System.out.println("Error");
         }
     }
 }
