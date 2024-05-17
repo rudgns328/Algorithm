27f81cecd3df7b1a2e99182eb9f3c5c366534564
rudgns328 <rudgns328@gmail.com>
2024-05-17 21:16:54 +0900

    [Improve]-[백준] 삼각형과 세 변 5073

--- src/백준/자바/기하학/삼각형과_세_변_5073/Main.java
+++ src/백준/자바/기하학/삼각형과_세_변_5073/Main.java
@@ -9,11 +9,11 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[3];
 
 
         while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             arr[0] = Integer.parseInt(st.nextToken());
             arr[1] = Integer.parseInt(st.nextToken());
             arr[2] = Integer.parseInt(st.nextToken());
@@ -22,12 +22,12 @@ public class Main {
 
             Arrays.sort(arr);
 
-            if (arr[0] + arr[1] < arr[2]) {
+            if (arr[0] + arr[1] <= arr[2]) {
                 System.out.println("Invalid");
             } else {
                 if (arr[0] == arr[1] && arr[0] == arr[2]) {
                     System.out.println("Equilateral");
-                } else if (arr[0] != arr[1] && arr[0] != arr[2]) {
+                } else if (arr[0] != arr[1] && arr[0] != arr[2] && arr[1] != arr[2]) {
                     System.out.println("Scalene");
                 } else System.out.println("Isosceles");
             }
