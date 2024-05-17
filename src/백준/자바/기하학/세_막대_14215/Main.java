db34f5179d134c3509070c040eb99eaefa44429e
rudgns328 <rudgns328@gmail.com>
2024-05-17 21:17:11 +0900

    [Improve]-[백준] 세 막대 14215

--- src/백준/자바/기하학/세_막대_14215/Main.java
+++ src/백준/자바/기하학/세_막대_14215/Main.java
@@ -3,27 +3,23 @@ package 백준.자바.기하학.세_막대_14215;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
 
-        int maxN = 0;
-        if(a>b){
-            if(b>c) maxN = a;
-            else maxN = (a>c) ? a : c;
-        }
-        else maxN = (b>c) ? b : c;
+        Arrays.sort(arr);
 
-        if(a+b+c-maxN > maxN) System.out.println(a+b+c);
-        else System.out.print((a+b+c-maxN)*2-1);
-
-        br.close();
+        if (arr[0] + arr[1] <= arr[2]) {
+            System.out.println((arr[0] + arr[1]) * 2 - 1);
+        } else System.out.println(arr[0] + arr[1] + arr[2]);
     }
 }
