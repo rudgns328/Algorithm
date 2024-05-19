f68038091b2c1bbc0f6720b815f763f9117df02a
rudgns328 <rudgns328@gmail.com>
2024-05-19 23:52:33 +0900

    [Improve]-[백준] 알고리즘 수업 - 알고리즘의 수행 시간 5 24266

--- src/백준/자바/시간복잡도/알고리즘_수업_알고리즘의_수행_시간_5_24266/Main.java
+++ src/백준/자바/시간복잡도/알고리즘_수업_알고리즘의_수행_시간_5_24266/Main.java
@@ -5,20 +5,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         long n = Long.parseLong(br.readLine());
         br.close();
 
-        int count = 0; //수행 홧수 저장하는 변수
-
-        for(int i  = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                for(int k = 1; k <= n; k++){
-                    count++;
-                }
-            }
-        }
-
-        System.out.println(count);
+        bw.write(n*n*n + "\n" + 3);
+        bw.flush();
+        bw.close();
     }
 }
