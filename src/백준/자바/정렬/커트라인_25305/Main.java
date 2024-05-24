3def334407bfffea3aba1dd4b4af3676f831f90d
rudgns328 <rudgns328@gmail.com>
2024-05-24 23:31:50 +0900

    [Improve]-[백준] 커트라인 25305

--- src/백준/자바/정렬/커트라인_25305/Main.java
+++ src/백준/자바/정렬/커트라인_25305/Main.java
@@ -8,25 +8,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st =  new StringTokenizer(br.readLine()); //첫번째 줄 입력받기
 
-        int N = Integer.parseInt(st.nextToken()); //응시자수
-        int k = Integer.parseInt(st.nextToken()); //커르라인 수
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] score = new int[N];
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        st =  new StringTokenizer(br.readLine()); // 두번째줄 입력 받기 위해
+        int[] arr = new int[N];
 
-        for(int i = 0; i < N; i++) { // 점수 5개 받기
-            score[i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         br.close();
 
-        Arrays.sort(score); //작은수 -> 큰수로 정렬
-
-        bw.write(score[N-k] +""); // 작은 수 -> 큰 수로 정렬했으므로 응시자수 - 커트라인 하면 해당 커트라인 점수가 된다.
+        Arrays.sort(arr);
+        bw.write(String.valueOf(arr[N - k]));
         bw.flush();
         bw.close();
-
     }
 }
