d5292f9415c24fa78f6c97038ee89ec25b9ff81a
rudgns328 <rudgns328@gmail.com>
2024-05-24 23:31:06 +0900

    [Improve]-[백준] 대표값2 2587

--- src/백준/자바/정렬/대표값2_2587/Main.java
+++ src/백준/자바/정렬/대표값2_2587/Main.java
@@ -5,21 +5,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[5];		// 입력한 값을 담을 배열 생성
-        int sum = 0;				// 총 합을 구할 변수 생성
+        int[] arr = new int[5];
 
-        for(int i=0; i<5; i++) {	// 반복문
-            int num = sc.nextInt(); // 숫자 입력받기
-            arr[i] += num;			// 배열에 숫자 넣기
-            sum += num;				// 평균을 구하기 위해 입력받은 수의 총합 구하기
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        System.out.println(sum/5);	// 총합을 5로 나누기
+        Arrays.sort(arr);
 
-        Arrays.sort(arr);			// 배열 정렬하기
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
 
-        System.out.println(arr[2]);	// 3번째 값이 중앙값
+        int avg = sum / 5;
 
-        sc.close();
+        System.out.println(avg);
+        System.out.println(arr[2]);
     }
 }
