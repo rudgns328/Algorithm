234a5ded2fa9dcb35971514d06457e333e4943be
rudgns328 <rudgns328@gmail.com>
2024-05-25 23:18:50 +0900

    [Solve]-[백준] 소트인사이드 1427

--- src/백준/자바/정렬/소트인사이드_1427/Main.java
+++ src/백준/자바/정렬/소트인사이드_1427/Main.java
@@ -1,2 +1,18 @@
-package 백준.자바.정렬.소트인사이드_1427;public class Main {
+package 백준.자바.정렬.소트인사이드_1427;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] arr = br.readLine().toCharArray();
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
 }
