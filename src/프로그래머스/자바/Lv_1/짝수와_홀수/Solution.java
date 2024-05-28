73ccf1a58c260d22ec72dbfc22c084429cf5c3b5
rudgns328 <rudgns328@gmail.com>
2024-05-28 23:55:18 +0900

    [Solve]-[프로그래머스] 짝수와 홀수

--- src/프로그래머스/자바/Lv_1/짝수와_홀수/Solution.java
+++ src/프로그래머스/자바/Lv_1/짝수와_홀수/Solution.java
@@ -1,2 +1,11 @@
-package 프로그래머스.자바.Lv_1.짝수와_홀수;public class Solution {
+package 프로그래머스.자바.Lv_1.짝수와_홀수;
+
+class Solution {
+    public String solution(int num) {
+        String ans = "";
+        if (num % 2 == 0) ans = "Even";
+        else ans = "Odd";
+
+        return ans;
+    }
 }
