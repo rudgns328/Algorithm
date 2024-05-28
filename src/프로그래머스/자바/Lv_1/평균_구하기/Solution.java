c311d4e8b0d9d7c8378300ccd89e3c01f57d31fa
rudgns328 <rudgns328@gmail.com>
2024-05-28 23:55:31 +0900

    [Solve]-[프로그래머스] 평균 구하기

--- src/프로그래머스/자바/Lv_1/평균_구하기/Solution.java
+++ src/프로그래머스/자바/Lv_1/평균_구하기/Solution.java
@@ -1,2 +1,12 @@
-package 프로그래머스.자바.Lv_1.평균_구하기;public class Solution {
+package 프로그래머스.자바.Lv_1.평균_구하기;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer/arr.length;
+    }
 }
