5f387cb23fd32dfb617ea281009a0efd78245814
rudgns328 <rudgns328@gmail.com>
2024-05-28 23:55:03 +0900

    [Solve]-[프로그래머스] 정수 제곱근 판별

--- src/프로그래머스/자바/Lv_1/정수_제곱근_판별/Solution.java
+++ src/프로그래머스/자바/Lv_1/정수_제곱근_판별/Solution.java
@@ -1,2 +1,13 @@
-package 프로그래머스.자바.Lv_1.정수_제곱근_판별;public class Solution {
+package 프로그래머스.자바.Lv_1.정수_제곱근_판별;
+
+class Solution {
+    public long solution(long n) {
+        double sqrt = Math.sqrt(n);
+        long ans = 0;
+
+        if (sqrt % 1 == 0) {
+            ans = (long) Math.pow(sqrt + 1, 2);
+        } else ans = -1;
+        return ans;
+    }
 }
