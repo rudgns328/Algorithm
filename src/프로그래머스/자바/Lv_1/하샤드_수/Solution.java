29bb30eba66084c3c6c2f8ad368ba86b18b011de
rudgns328 <rudgns328@gmail.com>
2024-05-28 23:55:46 +0900

    [Solve]-[프로그래머스] 하샤드 수

--- src/프로그래머스/자바/Lv_1/하샤드_수/Solution.java
+++ src/프로그래머스/자바/Lv_1/하샤드_수/Solution.java
@@ -1,2 +1,18 @@
-package 프로그래머스.자바.Lv_1.하샤드_수;public class Solution {
+package 프로그래머스.자바.Lv_1.하샤드_수;
+
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String[] list = String.valueOf(x).split("");
+        int sum = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            sum += Integer.parseInt(list[i]);
+        }
+
+        if (x % sum == 0) answer = true;
+        else answer = false;
+
+        return answer;
+    }
 }
