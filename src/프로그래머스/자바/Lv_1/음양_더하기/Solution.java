b45e7de4a2444e8398268b8c8312901fc81f13e6
rudgns328 <rudgns328@gmail.com>
2024-05-29 20:17:14 +0900

    [Solve]-[프로그래머스] 음양 더하기

--- src/프로그래머스/자바/Lv_1/음양_더하기/Solution.java
+++ src/프로그래머스/자바/Lv_1/음양_더하기/Solution.java
@@ -1,2 +1,18 @@
-package 프로그래머스.자바.Lv_1.음양_더하기;public class Solution {
+package 프로그래머스.자바.Lv_1.음양_더하기;
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i] == false) {
+                absolutes[i] = -1 * absolutes[i];
+            }
+        }
+
+        for (int i = 0; i < absolutes.length; i++) {
+            answer += absolutes[i];
+        }
+        return answer;
+    }
 }
