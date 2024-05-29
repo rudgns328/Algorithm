e7c7d3d01101eeb307f6af1d903cbdddd77b551f
rudgns328 <rudgns328@gmail.com>
2024-05-29 20:16:58 +0900

    [Solve]-[프로그래머스] 서울에서 김서방 찾기

--- src/프로그래머스/자바/Lv_1/서울에서_김서방_찾기/Solution.java
+++ src/프로그래머스/자바/Lv_1/서울에서_김서방_찾기/Solution.java
@@ -1,2 +1,15 @@
-package 프로그래머스.자바.Lv_1.서울에서_김서방_찾기;public class Solution {
+package 프로그래머스.자바.Lv_1.서울에서_김서방_찾기;
+
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int index = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                index = i;
+            }
+        }
+        answer = "김서방은 " + index + "에 있다";
+        return answer;
+    }
 }
