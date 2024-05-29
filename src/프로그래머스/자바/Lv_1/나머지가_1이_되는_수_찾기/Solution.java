7afd22c0883cd816ef8ec7f86eae564c2eb00cdb
rudgns328 <rudgns328@gmail.com>
2024-05-29 20:16:25 +0900

    [Solve]-[프로그래머스] 나머지가 1이 되는 수 찾기

--- src/프로그래머스/자바/Lv_1/나머지가_1이_되는_수_찾기/Solution.java
+++ src/프로그래머스/자바/Lv_1/나머지가_1이_되는_수_찾기/Solution.java
@@ -1,2 +1,15 @@
-package 프로그래머스.자바.Lv_1.나머지가_1이_되는_수_찾기;public class Solution {
+package 프로그래머스.자바.Lv_1.나머지가_1이_되는_수_찾기;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
 }
