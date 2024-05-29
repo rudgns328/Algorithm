ca2b5d2999006615e09f5090c9ae2065b026e43f
rudgns328 <rudgns328@gmail.com>
2024-05-29 20:16:43 +0900

    [Solve]-[프로그래머스] 두 정수 사이의 합

--- src/프로그래머스/자바/Lv_1/두_정수_사이의_합/Solution.java
+++ src/프로그래머스/자바/Lv_1/두_정수_사이의_합/Solution.java
@@ -1,2 +1,13 @@
-package 프로그래머스.자바.Lv_1.두_정수_사이의_합;public class Solution {
+package 프로그래머스.자바.Lv_1.두_정수_사이의_합;
+
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+        for (long i = min; i <= max; i++) {
+            answer += i;
+        }
+        return answer;
+    }
 }
