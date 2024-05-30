6e3e7cfc8a5f3852810dea532dcdcc136ff8bcce
rudgns328 <rudgns328@gmail.com>
2024-05-30 23:56:19 +0900

    [Solve]-[프로그래머스] 나누어 떨어지는 숫자 배열

--- src/프로그래머스/자바/Lv_1/나누어_떨어지는_숫자_배열/Solution.java
+++ src/프로그래머스/자바/Lv_1/나누어_떨어지는_숫자_배열/Solution.java
@@ -1,2 +1,25 @@
-package 프로그래머스.자바.Lv_1.나누어_떨어지는_숫자_배열;public class Solution {
+package 프로그래머스.자바.Lv_1.나누어_떨어지는_숫자_배열;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        List<Integer> list = new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                list.add(arr[i]);
+            }
+        }
+        if (list.isEmpty()) {
+            return new int[] { -1 };
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
 }
