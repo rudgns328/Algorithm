3d1baca189d0aa8f9e99d5abcfc5e2e840d0b22b
rudgns328 <rudgns328@gmail.com>
2024-05-30 23:56:48 +0900

    [Solve]-[프로그래머스] 제일 작은 수 제거하기

--- src/프로그래머스/자바/Lv_1/제일_작은_수_제거하기/Solution.java
+++ src/프로그래머스/자바/Lv_1/제일_작은_수_제거하기/Solution.java
@@ -1,2 +1,25 @@
-package 프로그래머스.자바.Lv_1.제일_작은_수_제거하기;public class Solution {
+package 프로그래머스.자바.Lv_1.제일_작은_수_제거하기;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        if (arr.length == 1) {
+            answer = new int[] {-1};
+        } else {
+            List<Integer> list = new ArrayList<>();
+            for (int a : arr) {
+                list.add(a);
+            }
+
+            list.remove(Collections.min(list));
+            answer = new int[arr.length - 1];
+
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = list.get(i);
+            }
+        }
+        return answer;
+    }
 }
