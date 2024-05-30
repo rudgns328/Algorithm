3cfc3188506cef611b643ace11e867a43e990f3b
rudgns328 <rudgns328@gmail.com>
2024-05-30 23:57:03 +0900

    [Solve]-[프로그래머스] 콜라츠 추측

--- src/프로그래머스/자바/Lv_1/콜라츠_추측/Solution.java
+++ src/프로그래머스/자바/Lv_1/콜라츠_추측/Solution.java
@@ -1,2 +1,22 @@
-package 프로그래머스.자바.Lv_1.콜라츠_추측;public class Solution {
+package 프로그래머스.자바.Lv_1.콜라츠_추측;
+
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num = num /2;
+            } else {
+                num = num * 3 + 1;
+            }
+            answer++;
+
+            if (answer >= 500) {
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
 }
