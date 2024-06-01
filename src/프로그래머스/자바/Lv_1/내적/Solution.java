139ffbe0920d99e9b122e799a471a688fefcaa35
rudgns328 <rudgns328@gmail.com>
2024-06-01 23:53:42 +0900

    [Solve]-[프로그래머스] 내적

--- src/프로그래머스/자바/Lv_1/내적/Solution.java
+++ src/프로그래머스/자바/Lv_1/내적/Solution.java
@@ -1,2 +1,12 @@
-package 프로그래머스.자바.Lv_1.내적;public class Solution {
+package 프로그래머스.자바.Lv_1.내적;
+
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
 }
