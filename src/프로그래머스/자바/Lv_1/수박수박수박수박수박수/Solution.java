ba56f239a74560ccd469e3eb5ced0f7fd15fbe2b
rudgns328 <rudgns328@gmail.com>
2024-06-01 23:53:22 +0900

    [Solve]-[프로그래머스] 수박수박수박수박수박수?

--- src/프로그래머스/자바/Lv_1/수박수박수박수박수박수/Solution.java
+++ src/프로그래머스/자바/Lv_1/수박수박수박수박수박수/Solution.java
@@ -1,2 +1,28 @@
-package 프로그래머스.자바.Lv_1.수박수박수박수박수박수;public class Solution {
+package 프로그래머스.자바.Lv_1.수박수박수박수박수박수;
+
+class Solution {
+    public String solution(int n) {
+        StringBuffer sb = new StringBuffer();
+        String str = "수박";
+
+        String answer = "";
+
+        if (n == 1) answer = "수";
+
+        if (n % 2 == 0) {
+            for (int i = 0; i < n / 2; i++) {
+                sb.append(str);
+            }
+        } else {
+            for (int j = 0; j < n; j++) {
+                sb.append(str);
+            }
+            for (int k = sb.length() - 1; k >= n; k--) {
+                sb.deleteCharAt(k);
+            }
+        }
+
+        answer = String.valueOf(sb);
+        return answer;
+    }
 }
