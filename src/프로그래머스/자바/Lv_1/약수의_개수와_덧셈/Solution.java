2c41fd6c82ee619631b50ba5a1d002256298c762
rudgns328 <rudgns328@gmail.com>
2024-06-01 23:54:04 +0900

    [Solve]-[프로그래머스] 약수의 개수와 덧셈

--- src/프로그래머스/자바/Lv_1/약수의_개수와_덧셈/Solution.java
+++ src/프로그래머스/자바/Lv_1/약수의_개수와_덧셈/Solution.java
@@ -1,2 +1,15 @@
-package 프로그래머스.자바.Lv_1.약수의_개수와_덧셈;public class Solution {
+package 프로그래머스.자바.Lv_1.약수의_개수와_덧셈;
+
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for (int i = left; i <= right; i++) {
+            if (i % Math.sqrt(i) == 0) {
+                answer -= i;
+            } else answer += i;
+        }
+
+        return answer;
+    }
 }
