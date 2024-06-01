a713455043239a58cedec618cf1c0635feda3f8c
rudgns328 <rudgns328@gmail.com>
2024-06-01 23:54:32 +0900

    [Solve]-[프로그래머스] 문자열 내림차순으로 배치하기

--- src/프로그래머스/자바/Lv_1/문자열_내림차순으로_배치하기/Solution.java
+++ src/프로그래머스/자바/Lv_1/문자열_내림차순으로_배치하기/Solution.java
@@ -1,2 +1,16 @@
-package 프로그래머스.자바.Lv_1.문자열_내림차순으로_배치하기;public class Solution {
+package 프로그래머스.자바.Lv_1.문자열_내림차순으로_배치하기;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] list = s.split("");
+        Arrays.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for (String aList : list) {
+            sb.append(aList);
+        }
+        return String.valueOf(sb.reverse());
+    }
 }
