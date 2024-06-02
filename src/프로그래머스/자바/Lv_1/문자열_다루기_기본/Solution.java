7b11ec853ba8921a97490ca9f3ee69e1f0b020a5
rudgns328 <rudgns328@gmail.com>
2024-06-02 20:27:57 +0900

    [Solve]-[프로그래머스] 문자열 다루기 기본

--- src/프로그래머스/자바/Lv_1/문자열_다루기_기본/Solution.java
+++ src/프로그래머스/자바/Lv_1/문자열_다루기_기본/Solution.java
@@ -1,2 +1,16 @@
-package 프로그래머스.자바.Lv_1.문자열_다루기_기본;public class Solution {
+package 프로그래머스.자바.Lv_1.문자열_다루기_기본;
+
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if (s.length() != 4 && s.length() != 6) {
+            answer = false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') {
+                answer = false;
+            }
+        }
+        return answer;
+    }
 }
