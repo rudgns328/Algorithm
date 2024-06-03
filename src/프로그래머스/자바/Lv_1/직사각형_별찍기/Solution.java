0dc4d1821ff28b41305aa4ec888f15f4a69b7034
rudgns328 <rudgns328@gmail.com>
2024-06-03 23:22:18 +0900

    [Solve]-[프로그래머스] 직사각형 별찍기

--- src/프로그래머스/자바/Lv_1/직사각형_별찍기/Solution.java
+++ src/프로그래머스/자바/Lv_1/직사각형_별찍기/Solution.java
@@ -1,2 +1,18 @@
-package 프로그래머스.자바.Lv_1.직사각형_별찍기;public class Solution {
+package 프로그래머스.자바.Lv_1.직사각형_별찍기;
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
