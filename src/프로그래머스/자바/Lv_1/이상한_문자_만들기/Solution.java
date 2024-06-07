bce8a768979560d7c7641656f026981c91a55ef8
rudgns328 <rudgns328@gmail.com>
2024-06-07 23:37:49 +0900

    [Solve]-[프로그래머스] 이상한 문자 만들기

--- src/프로그래머스/자바/Lv_1/이상한_문자_만들기/Solution.java
+++ src/프로그래머스/자바/Lv_1/이상한_문자_만들기/Solution.java
@@ -1,2 +1,25 @@
-package 프로그래머스.자바.Lv_1.이상한_문자_만들기;public class Solution {
-}
+package 프로그래머스.자바.Lv_1.이상한_문자_만들기;
+
+public class Solution {
+        public String solution(String s) {
+            String answer = "";
+            String[] str = s.split("");
+
+            int idx = 0;
+            for(int i=0; i<str.length; i++) {
+                if(str[i].equals(" ")) {
+                    idx = 0;
+                } else if(idx % 2 == 0) {
+                    str[i] = str[i].toUpperCase();
+                    idx++;
+                } else if(idx % 2 != 0) {
+                    str[i] = str[i].toLowerCase();
+                    idx++;
+                }
+                answer += str[i];
+            }
+
+            return answer;
+        }
+    }
+
