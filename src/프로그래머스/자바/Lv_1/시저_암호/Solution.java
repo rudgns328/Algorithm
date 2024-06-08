a4e4fb71da8eb22e5e13f65a10ccf188a7f54448
rudgns328 <rudgns328@gmail.com>
2024-06-08 23:49:36 +0900

    [Solve]-[프로그래머스] 시저 암호

--- src/프로그래머스/자바/Lv_1/시저_암호/Solution.java
+++ src/프로그래머스/자바/Lv_1/시저_암호/Solution.java
@@ -1,2 +1,29 @@
-package 프로그래머스.자바.Lv_1.시저_암호;public class Solution {
+package 프로그래머스.자바.Lv_1.시저_암호;
+
+public class Solution {
+
+    public String solution(String s, int n) {
+        String answer = "";
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch==' ') { //공백
+                answer += ch;
+                continue;
+            }
+            if(ch>='a' && ch<='z') { //소문자
+                if(ch+n > 'z') {
+                    answer += (char)(ch-26+n);
+                }else {
+                    answer += (char)(ch+n);
+                }
+            }else if(ch>='A' && ch<='Z') { //대문자
+                if(ch+n > 'Z') {
+                    answer += (char)(ch-26+n);
+                }else {
+                    answer += (char)(ch+n);
+                }
+            }
+        }
+        return answer;
+    }
 }
