ad2c57e4257e95e6f9f6dfc9a1f7b4385adba5a5
rudgns328 <rudgns328@gmail.com>
2024-06-11 21:58:22 +0900

    [Solve]-[프로그래머스] 푸드 파이터 대회

--- src/프로그래머스/자바/Lv_1/푸드_파이터_대회/Solution.java
+++ src/프로그래머스/자바/Lv_1/푸드_파이터_대회/Solution.java
@@ -1,2 +1,19 @@
-package 프로그래머스.자바.Lv_1.푸드_파이터_대회;public class Solution {
+package 프로그래머스.자바.Lv_1.푸드_파이터_대회;
+class Solution {
+    public String solution(int[] food) {
+        // 음식을 순서대로 담을 StringBuilder 선언
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < food.length; i++) {
+            // i번 음식을 2로 나눈 개수만큼 StringBuilder에 추가
+            int count = food[i] / 2;
+            sb.append(String.valueOf(i).repeat(count));
+        }
+
+        // StringBuilder에 추가된 음식에 0과 reverse된 문자열 더하기
+        String answer = sb + "0";
+        answer += sb.reverse();
+
+        return answer;
+    }
 }
