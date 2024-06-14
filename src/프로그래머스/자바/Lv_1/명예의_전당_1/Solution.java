dd49dd6455b7125b91d793b3d21c3ffd97a0babb
rudgns328 <rudgns328@gmail.com>
2024-06-14 21:59:25 +0900

    [Solve]-[프로그래머스] 명예의 전당 (1)

--- src/프로그래머스/자바/Lv_1/명예의_전당_1/Solution.java
+++ src/프로그래머스/자바/Lv_1/명예의_전당_1/Solution.java
@@ -1,2 +1,30 @@
-package 프로그래머스.자바.Lv_1.명예의_전당_1;public class Solution {
+package 프로그래머스.자바.Lv_1.명예의_전당_1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        // 결과를 담을 배열 선언
+        int[] answer = new int[score.length];
+
+        // 순위를 담을 List 선언
+        List<Integer> rank = new ArrayList<>();
+
+        // score를 반복
+        for (int i = 0; i < score.length; i++) {
+            // score 추가
+            rank.add(score[i]);
+
+            // rank 리스트의 크기가 k보다 크면 가장 낮은 점수 제거
+            if (rank.size() > k) {
+                rank.remove(Collections.min(rank));
+            }
+
+            // 남은 점수 중 가장 작은 점수 answer에 담기
+            answer[i] = Collections.min(rank);
+        }
+        return answer;
+    }
 }
