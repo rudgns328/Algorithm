999e2ad90195c1c9f93b3250ed08f679e3a5f44d
rudgns328 <rudgns328@gmail.com>
2024-06-16 16:45:05 +0900

    [Solve]-[프로그래머스] 2016년

--- src/프로그래머스/자바/Lv_1/_2016년/Solution.java
+++ src/프로그래머스/자바/Lv_1/_2016년/Solution.java
@@ -1,2 +1,16 @@
-package 프로그래머스.자바.Lv_1._2016년;public class Solution {
+package 프로그래머스.자바.Lv_1._2016년;
+
+class Solution {
+    public String solution(int a, int b) {
+        String[] days = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        int[] months = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+        int total = 0; // 총 일수
+
+        for(int i=0; i<a; i++) {
+            total += months[i];
+        }
+        total += b;
+
+        return days[(total + 4) % 7];
+    }
 }
