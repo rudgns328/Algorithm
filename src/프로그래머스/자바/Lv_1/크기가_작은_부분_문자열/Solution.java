8d3a682763854cbdc07a4ebc14f433c5a300d3ad
rudgns328 <rudgns328@gmail.com>
2024-06-18 23:44:29 +0900

    [Improve]-[프로그래머스] 크기가 작은 부분 문자열

--- src/프로그래머스/자바/Lv_1/크기가_작은_부분_문자열/Solution.java
+++ src/프로그래머스/자바/Lv_1/크기가_작은_부분_문자열/Solution.java
@@ -1,22 +1,15 @@
 package 프로그래머스.자바.Lv_1.크기가_작은_부분_문자열;
 
-public class Solution {
+class Solution {
     public int solution(String t, String p) {
-        int len = p.length();
-        long num = Long.parseLong(p);
-        int result = 0;
+        int answer = 0;
 
-        for (int i = 0; i < t.length() - len + 1; i++) {
-            long diff = Long.parseLong(t.substring(i, i + len));
-            if (diff <= num) result++;
+        for (int i = 0; i <= t.length() - p.length(); i++) {
+            String a = t.substring(i, i + p.length());
+            if (Long.parseLong(a) <= Long.parseLong(p)) {
+                answer++;
+            }
         }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        Solution smallSubstring = new Solution();
-        System.out.println(smallSubstring.solution("3141592", "271"));  // 2
-        System.out.println(smallSubstring.solution("500220839878", "7"));  // 8
-        System.out.println(smallSubstring.solution("10203", "15"));  // 3
+        return answer;
     }
 }
