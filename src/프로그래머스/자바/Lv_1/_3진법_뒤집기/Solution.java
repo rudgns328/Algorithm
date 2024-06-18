3bad1fe2bed31270b391cbd35f63c5ef230a3df7
rudgns328 <rudgns328@gmail.com>
2024-06-18 23:44:11 +0900

    [Improve]-[프로그래머스] 3진법 뒤집기

--- src/프로그래머스/자바/Lv_1/_3진법_뒤집기/Solution.java
+++ src/프로그래머스/자바/Lv_1/_3진법_뒤집기/Solution.java
@@ -2,13 +2,13 @@ package 프로그래머스.자바.Lv_1._3진법_뒤집기;
 
 class Solution {
     public int solution(int n) {
-        String str = "";
+        String three = Integer.toString(n, 3);
+        StringBuilder sb = new StringBuilder(three);
+        String ans = String.valueOf(sb.reverse());
 
-        while (n != 0) {
-            str += n % 3;
-            n /= 3;
-        }
+        int answer = Integer.parseInt(ans, 3);
 
-        return Integer.parseInt(str, 3);
+
+        return answer;
     }
 }
