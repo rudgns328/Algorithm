77d75dbf5f395d59b69b7cb840b3090cfe3e96e6
rudgns328 <rudgns328@gmail.com>
2024-06-19 23:53:46 +0900

    [Improve]-[프로그래머스] 예산

--- src/프로그래머스/자바/Lv_1/예산/Solution.java
+++ src/프로그래머스/자바/Lv_1/예산/Solution.java
@@ -1,17 +1,16 @@
 package 프로그래머스.자바.Lv_1.예산;
 
-import java.util.Arrays;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int total = 0;
-        Arrays.sort(d);//오름차순 정렬
-        for(int i=0;i<d.length; i++){
-            if(total+d[i]<=budget) {
-                total += d[i]; //배열의 값을 하나 씩 더한다
-                answer++;
-            }
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            if (budget < d[i]) break;
+            budget -= d[i];
+            answer++;
         }
         return answer;
     }
