515170a052a414a7b8b53bf7dbbc8e1379cb26f9
rudgns328 <rudgns328@gmail.com>
2024-06-19 23:54:36 +0900

    [Improve]-[프로그래머스] 최소직사각형

--- src/프로그래머스/자바/Lv_1/최소직사각형/Solution.java
+++ src/프로그래머스/자바/Lv_1/최소직사각형/Solution.java
@@ -3,15 +3,16 @@ package 프로그래머스.자바.Lv_1.최소직사각형;
 import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        int max_v=0;
-        int max_h=0;
-        for(int i=0;i<sizes.length;i++){
-            int v=Math.max(sizes[i][0],sizes[i][1]);
-            int h=Math.min(sizes[i][0],sizes[i][1]);
-            max_v=Math.max(max_v,v);
-            max_h=Math.max(max_h,h);
+        int max_w = 0;
+        int max_h = 0;
+
+        for (int i = 0; i < sizes.length; i++) {
+            int w = Math.max(sizes[i][0], sizes[i][1]);
+            int h = Math.min(sizes[i][0], sizes[i][1]);
+
+            max_w = Math.max(max_w, w);
+            max_h = Math.max(max_h, h);
         }
-        return answer=max_v*max_h;
+        return max_w * max_h;
     }
 }
