786519ea0942436384998e6b38749cb23ab19bac
rudgns328 <rudgns328@gmail.com>
2024-06-19 23:54:25 +0900

    [Improve]-[프로그래머스] 삼총사

--- src/프로그래머스/자바/Lv_1/삼총사/Solution.java
+++ src/프로그래머스/자바/Lv_1/삼총사/Solution.java
@@ -4,10 +4,10 @@ class Solution {
     public int solution(int[] number) {
         int answer = 0;
 
-        for(int i = 0; i < number.length; i++) {
-            for(int j = i + 1; j < number.length; j++) {
-                for(int k = j + 1; k < number.length; k++) {
-                    if(number[i] + number[j] + number[k] == 0) {
+        for (int i = 0; i < number.length - 2; i++) {
+            for (int j = i; j < number.length - 2; j++) {
+                for (int k = j; k < number.length - 2; k++) {
+                    if (number[i] + number[j + 1] + number[k + 2] == 0) {
                         answer++;
                     }
                 }
