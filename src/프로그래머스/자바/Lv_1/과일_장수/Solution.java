95911db367668b2cd7fdebb45347d6211fc22af5
rudgns328 <rudgns328@gmail.com>
2024-06-23 23:08:52 +0900

    [Solve]-[프로그래머스] 과일 장수

--- src/프로그래머스/자바/Lv_1/과일_장수/Solution.java
+++ src/프로그래머스/자바/Lv_1/과일_장수/Solution.java
@@ -1,2 +1,24 @@
-package 프로그래머스.자바.Lv_1.과일_장수;public class Solution {
+package 프로그래머스.자바.Lv_1.과일_장수;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        int boxNum = score.length / m;
+        Arrays.sort(score);
+
+        if(score.length % m == 0) {
+            for(int i = 0; i < boxNum; i++) {
+                answer += (score[i * m]) * m * 1;
+            }
+        } else {
+            int rest = score.length % m;
+            for(int i = 0; i < boxNum; i++) {
+                answer += (score[rest + (i * m)]) * m * 1;
+            }
+        }
+
+        return answer;
+    }
 }
