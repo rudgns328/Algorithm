78fb0853073a2c383f025e2cd12a755650638bdf
[CLOUD4] 송경훈 <rudgns328@gmail.com>
2024-07-01 23:01:22 +0900

    [Solve]-[프로그래머스] 문자열 나누기

--- src/프로그래머스/자바/Lv_1/문자열_나누기/Solution.java
+++ src/프로그래머스/자바/Lv_1/문자열_나누기/Solution.java
@@ -1,2 +1,25 @@
-package 프로그래머스.자바.Lv_1.문자열_나누기;public class Solution {
+package 프로그래머스.자바.Lv_1.문자열_나누기;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 1;
+
+        char x = s.charAt(0);
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (count == 0) {
+                answer++;
+                x = s.charAt(i);
+            }
+
+            if (x == s.charAt(i)) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+
+        return answer;
+    }
 }
