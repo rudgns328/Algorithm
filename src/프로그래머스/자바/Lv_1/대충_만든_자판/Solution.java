1dca2427e492a7b6bd5999ff366cf226b8755774
[CLOUD4] 송경훈 <rudgns328@gmail.com>
2024-07-02 23:01:00 +0900

    [Solve]-[프로그래머스] 둘만의 암호

---
+++ src/프로그래머스/자바/Lv_1/대충_만든_자판/Solution.java
@@ -0,0 +1,2 @@
+package 프로그래머스.자바.Lv_1.대충_만든_자판;public class Soltuion {
+}
---
+++ src/프로그래머스/자바/Lv_1/둘만의_암호/Solution.java
@@ -0,0 +1,24 @@
+package 프로그래머스.자바.Lv_1.둘만의_암호;
+
+class Solution {
+
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            for (int j = 0; j < index; j++) {
+                c += 1;
+                if (c > 'z') {
+                    c -= 26;
+                }
+                if (skip.contains(String.valueOf(c))) {
+                    j--;
+                }
+            }
+            answer += c;
+        }
+
+        return answer;
+    }
+}
