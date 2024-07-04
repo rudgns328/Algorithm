21331c64416c7ab9d5d5d34b812cb78b827b2420
[CLOUD4] 송경훈 <rudgns328@gmail.com>
2024-07-04 23:37:31 +0900

    [Improve]-[프로그래머스] 햄버거 만들기

---
+++ src/프로그래머스/자바/Lv_1/크레인_인형뽑기_게임/Solution.java
@@ -0,0 +1,2 @@
+package 프로그래머스.자바.Lv_1.크레인_인형뽑기_게임;public class Solution {
+}
--- src/프로그래머스/자바/Lv_1/햄버거_만들기/Solution.java
+++ src/프로그래머스/자바/Lv_1/햄버거_만들기/Solution.java
@@ -1,4 +1,25 @@
 package 프로그래머스.자바.Lv_1.햄버거_만들기;
 
-public class Solution {
+import java.util.*;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        List<Integer> li = new ArrayList<>();
+
+        for(int i :ingredient) {
+            li.add(i);
+            while(li.size() >= 4) {
+                int n = li.size();
+                if(!(li.get(n-1) == 1
+                        && li.get(n-2)==3
+                        && li.get(n-3) ==2
+                        && li.get(n-4)==1)) break;
+                for(int j=0; j<4; j++) {
+                    li.remove(li.size() -1);
+                }
+                answer++;
+            }
+        }
+        return answer;
+    }
 }
