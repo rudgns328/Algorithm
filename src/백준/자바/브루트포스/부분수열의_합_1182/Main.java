03c2fbe51eab791c351c58b7e0dba4baf54e5165
rudgns328 <rudgns328@gmail.com>
2024-07-27 21:22:11 +0900

    [Solve]-[백준] 부분수열의 합 1182

--- src/백준/자바/브루트포스/부분수열의_합_1182/Main.java
+++ src/백준/자바/브루트포스/부분수열의_합_1182/Main.java
@@ -1,2 +1,44 @@
-package 백준.자바.브루트포스.부분수열의_합_1182;public class Main {
+package 백준.자바.브루트포스.부분수열의_합_1182;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, S, count;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        count = 0;
+        dfs(0, 0);
+
+        if (S == 0) {
+            System.out.println(count - 1);
+        } else {
+            System.out.println(count);
+        }
+    }
+
+    static void dfs(int index, int sum) {
+        if (N == index) {
+            if (S == sum) {
+                count++;
+            }
+            return;
+        }
+
+        dfs(index + 1, sum + arr[index]);
+        dfs(index + 1, sum);
+    }
 }
