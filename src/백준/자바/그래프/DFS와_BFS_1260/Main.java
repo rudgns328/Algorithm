5fff6b761d78277ec111a08a8bcfaa6b35d97751
rudgns328 <rudgns328@gmail.com>
2024-08-04 23:25:20 +0900

    [Solve]-[백준] DFS와 BFS (인접행렬 사용) 1260

--- src/백준/자바/그래프/DFS와_BFS_1260/Main.java
+++ src/백준/자바/그래프/DFS와_BFS_1260/Main.java
@@ -1,2 +1,68 @@
-package 백준.자바.그래프.DFS와_BFS_1260;public class Main {
+package 백준.자바.그래프.DFS와_BFS_1260;
+
+import java.util.*;
+// 인접행렬
+public class Main {
+    static int N, M, V; // 정점갯수, 간선갯수, 시작정점
+    static boolean[][] adj; // 정점간의 인접 여부
+    static boolean[] visit; // 정점의 재방문 방지하기 위한 방문체크
+    static StringBuilder sb; // DFS 탐색시 재귀 함수에서 방문처리 해야해서 스트링 이어붙일 때 메소드 밖에 있어야 편할 수 있음
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        V = sc.nextInt();
+
+        adj = new boolean[N+1][N+1]; // 정점 번호가 1~N까지 사용
+
+        for (int m = 0; m < M; m++) {
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+
+            adj[v1][v2] = true;
+            adj[v2][v1] = true;
+        }
+
+        visit = new boolean[N+1]; // 1~N 번까지의 정점이 방문했는지 아닌지
+        sb = new StringBuilder(); // 방문 할 때 마다 여기 기록해야지
+        dfs(V);
+        System.out.println(sb.toString());
+
+        // bfs 탐색 출발을 위해 리셋!
+        visit = new boolean[N+1]; // 1~N 번까지의 정점이 방문했는지 아닌지
+        sb = new StringBuilder(); // 방문 할 때 마다 여기 기록해야지
+        bfs(V);
+        System.out.println(sb.toString());
+    }
+
+    public static void dfs(int now) {
+        visit[now] = true; // now 칸 방문했다!!
+        sb.append(now + " "); // 결과에 보여줄 방문처리 스트링 만들고!
+
+        for (int next = 1; next <= N; next++) {
+            if (adj[now][next] && !visit[next]) { // 현재 정점에서 인접한 다른 정점이 있어? 그럼 일단 여기서 난 기다려!!
+                dfs(next);
+            }
+        }
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> queue = new ArrayDeque<>(); // or new LinkedList
+        queue.add(start); // queue 가 곧 방문 스케줄
+        visit[start] = true; // BFS는 일단 스케줄 순서대로 무조건 방문하니까 스케줄 잡은 정점은 이미 큐에 먼저 들어갔다고 표시하기
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll(); // 현재 방문처리하는 정점
+            sb.append(now + " ");
+
+            for (int next = 1; next <= N; next++) {
+                if (adj[now][next] && !visit[next]) {
+                    queue.add(next);
+                    visit[next] = true;
+                }
+            }
+        }
+    }
 }
