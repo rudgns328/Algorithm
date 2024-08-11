5fee7ea89111f79153853fd3664b5e40ad17c4bb
rudgns328 <rudgns328@gmail.com>
2024-08-11 23:34:20 +0900

    [Solve]-[백준] solved.ac 18110

--- src/백준/자바/구현/solved_ac_18110/Main.java
+++ src/백준/자바/구현/solved_ac_18110/Main.java
@@ -1,2 +1,29 @@
-package 백준.자바.구현.solved_ac_18110;public class Main {
+package 백준.자바.구현.solved_ac_18110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        int trim = (int) Math.round(n * 0.15);
+
+        double sum = 0;
+        for (int i = trim; i < n - trim; i++) {
+            sum += arr[i];
+        }
+
+        int avg = (int) Math.round(sum / (n - trim * 2));
+        System.out.print(avg);
+    }
 }
