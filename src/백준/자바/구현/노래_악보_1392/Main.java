897ed247ba08899924f2e1ea15912b88c94b6f2c
rudgns328 <rudgns328@gmail.com>
2024-08-11 23:33:57 +0900

    [Solve]-[백준] 노래 악보 1392

--- src/백준/자바/구현/노래_악보_1392/Main.java
+++ src/백준/자바/구현/노래_악보_1392/Main.java
@@ -1,2 +1,36 @@
-package 백준.자바.구현.노래_악보_1392;public class Main {
+package 백준.자바.구현.노래_악보_1392;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            int time = Integer.parseInt(br.readLine());
+
+            for (int j = 0; j < time; j++) {
+                list.add(i);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < Q; i++) {
+            int questionTime = Integer.parseInt(br.readLine());
+
+            sb.append(list.get(questionTime)).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
