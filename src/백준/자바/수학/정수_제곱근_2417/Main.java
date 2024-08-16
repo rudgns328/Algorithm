f869402e66a019280dcadc0f54a4a64b6c1e08ec
rudgns328 <rudgns328@gmail.com>
2024-08-16 15:17:20 +0900

    [Solve]-[백준] 정수 제곱근 2417

--- src/백준/자바/수학/정수_제곱근_2417/Main.java
+++ src/백준/자바/수학/정수_제곱근_2417/Main.java
@@ -1,2 +1,16 @@
-package 백준.자바.수학.정수_제곱근_2417;public class Main {
+package 백준.자바.수학.정수_제곱근_2417;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long q = (long) Math.sqrt(n);
+
+        if ((q * q) < n) q++;
+        System.out.println(q);
+    }
 }
