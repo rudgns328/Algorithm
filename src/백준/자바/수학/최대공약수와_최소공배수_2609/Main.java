6aad1b53c3b85c6f84201064ddaaa15468fbf77f
rudgns328 <rudgns328@gmail.com>
2024-08-16 15:17:42 +0900

    [Solve]-[백준] 최대공약수와 최소공배수 2609

--- src/백준/자바/수학/최대공약수와_최소공배수_2609/Main.java
+++ src/백준/자바/수학/최대공약수와_최소공배수_2609/Main.java
@@ -1,2 +1,30 @@
-package 백준.자바.수학.최대공약수와_최소공배수_2609;public class Main {
+package 백준.자바.수학.최대공약수와_최소공배수_2609;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+
+        System.out.println(gcd(max, min));
+        System.out.println(lcm(max, min));
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
 }
