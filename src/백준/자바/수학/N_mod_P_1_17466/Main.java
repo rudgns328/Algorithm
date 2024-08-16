49b7a57113e5e7e86cc45449331121364bd06d31
rudgns328 <rudgns328@gmail.com>
2024-08-16 15:16:56 +0900

    [Solve]-[백준] N! mod P (1) 17466

--- src/백준/자바/수학/N_mod_P_1_17466/Main.java
+++ src/백준/자바/수학/N_mod_P_1_17466/Main.java
@@ -1,2 +1,20 @@
-package 백준.자바.수학.N_mod_P_1_17466;public class Main {
+package 백준.자바.수학.N_mod_P_1_17466;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextLong();
+        long p = scanner.nextLong();
+
+        long num = 1;
+
+        for (int i = 2; i <= n; i++) {
+            num = (num * i) % p;
+        }
+
+        System.out.println(num);
+    }
 }
