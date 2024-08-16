fbd2ab56deb50e37b673805ec05326358f75f9ff
rudgns328 <rudgns328@gmail.com>
2024-08-16 15:16:16 +0900

    [Solve]-[백준] 2의 제곱인가? 11966

--- src/백준/자바/수학/_2의_제곱인가_11966/Main.java
+++ src/백준/자바/수학/_2의_제곱인가_11966/Main.java
@@ -1,2 +1,19 @@
-package 백준.자바.수학._2의_제곱인가;public class Main {
+package 백준.자바.수학._2의_제곱인가_11966;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        while (N != 1) {
+            if (N % 2 == 1) {
+                System.out.println(0);
+                return;
+            }
+            N /= 2;
+        }
+        System.out.println(1);
+    }
 }
