3047a8c56fbd76bc8764ca1c39f5ae0bf8ede092
rudgns328 <rudgns328@gmail.com>
2024-08-28 23:33:31 +0900

    [Solve]-[백준] 반올림 4539

--- src/백준/자바/구현/반올림_4539/Main.java
+++ src/백준/자바/구현/반올림_4539/Main.java
@@ -1,2 +1,34 @@
-package 백준.자바.구현.반올림_4539;public class Main {
+package 백준.자바.구현.반올림_4539;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x > 10) {
+                x = ((x + 5) / 10) * 10;
+            }  if (x > 100) {
+                x = ((x + 50) / 100) * 100;
+            }  if (x > 1000) {
+                x = ((x + 500) / 1000) * 1000;
+            }  if (x > 10000) {
+                x = ((x + 5000) / 10000) * 10000;
+            }  if (x > 100000) {
+                x = ((x + 50000) / 100000) * 100000;
+            }  if (x > 1000000) {
+                x = ((x + 500000) / 1000000) * 1000000;
+            }  if (x > 10000000) {
+                x = ((x + 5000000) / 10000000) * 10000000;
+            }
+
+            System.out.println(x);
+        }
+    }
 }
